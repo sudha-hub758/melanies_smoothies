44c809d4dc9041e1699816bd1290f3b6c3d9a9aa
sudha-hub758 <sudha.mani@genpact.com>
2024-12-09 04:54:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import pands 
+import pandas 
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
