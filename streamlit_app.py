932bf08b304ec9a95da984348d766349924802f8
sudha-hub758 <sudha.mani@genpact.com>
2024-12-09 05:06:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', each_fruit,' is ', search_on, '.')
         st.subheader(each_fruit + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + each_fruit)
